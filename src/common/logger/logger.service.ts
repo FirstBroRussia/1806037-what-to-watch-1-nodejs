70d0b4d1d68ed5d17a80703237b4a79a27d6df7d
FirstBroRussia <chipanrussia@gmail.com>
2022-09-29 20:20:11 +0300

    Тут с помощью mongoose создана модель с имплементацией, наследование от типов и применена схема

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -6,7 +6,7 @@ import { LoggerInterface } from "./logger.interface.js";
 
 @injectable()
 export default class LoggerService implements LoggerInterface {
-	constructor(private logger: Logger = pino()) { }
+	constructor(private logger: Logger = pino()) { 	}
 
 	public debug(message: string, ...args: unknown[]): void {
 		this.logger.debug(message, ...args);
---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { User } from "../../types/user.type";
+
+export interface UserDocument extends User, mongoose.Document {
+    createdAt: Date,
+    updateAt: Date,
+};
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: [1, 'Min length for username is 1'],
+        maxlength: [15, 'Min length for username is 15']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4}])?$/, 'Email is incorrect'],
+    },
+    avatarPath: {
+        type: String,
+        required: false,
+        match: [/(^.*.jpg$)|(^.*.png$)/, 'Image format only is .jpg and .png'],
+    },
+    // password: String,
+}, {
+    timestamps: true,
+});
+
+export const UserModel = mongoose.model<UserDocument>('User', userSchema);
